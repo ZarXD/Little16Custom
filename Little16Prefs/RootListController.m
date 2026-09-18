b112d6880c2c0033ab58370beef8bf2fcaf50cbe
ZarXD <banghads09@gmail.com>
2026-09-19 00:26:28 +0700

    Add on-device debug logging to prefs bundle (logs to /tmp/little16debug.log)

--- Little16Prefs/RootListController.m
+++ Little16Prefs/RootListController.m
@@ -2,19 +2,65 @@
 #import <Preferences/PSSpecifier.h>
 #include <spawn.h>
 
+static void L16Log(NSString *format, ...) {
+    va_list args;
+    va_start(args, format);
+    NSString *message = [[NSString alloc] initWithFormat:format arguments:args];
+    va_end(args);
+    NSData *data = [[message stringByAppendingString:@"\n"] dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *path = @"/tmp/little16debug.log";
+    if (![[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        [data writeToFile:path atomically:YES];
+    } else {
+        NSFileHandle *fh = [NSFileHandle fileHandleForWritingAtPath:path];
+        [fh seekToEndOfFile];
+        [fh writeData:data];
+        [fh closeFile];
+    }
+    NSLog(@"Little16Prefs: %@", message);
+}
+
 @interface Little16PrefsListController : PSListController
 @end
 
-@implementation Little16PrefsListController
+@implementation Little16PrefsListController {
+    BOOL _logged;
+}
+
+- (instancetype)init {
+    L16Log(@"init entered");
+    self = [super init];
+    if (self) L16Log(@"init exit ok");
+    return self;
+}
+
+- (void)loadView {
+    L16Log(@"loadView entered");
+    [super loadView];
+    L16Log(@"loadView exit");
+}
+
+- (void)viewDidLoad {
+    L16Log(@"viewDidLoad entered");
+    [super viewDidLoad];
+    L16Log(@"viewDidLoad exit");
+}
 
 - (NSArray *)specifiers {
+    L16Log(@"specifiers entered");
     if (!_specifiers) {
         _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+        L16Log(@"specifiers loaded count=%lu", (unsigned long)[_specifiers count]);
+    }
+    if (!_logged) {
+        _logged = YES;
+        L16Log(@"bundle=%@ exec=%@ cwd=%@", [[NSBundle mainBundle] bundleIdentifier], [[[NSBundle mainBundle] executablePath] lastPathComponent], [[NSFileManager defaultManager] currentDirectoryPath]);
     }
     return _specifiers;
 }
 
 - (void)respring {
+    L16Log(@"respring tapped");
     NSString *killall = nil;
     NSArray *candidates = @[@"/var/jb/usr/bin/killall", @"/usr/bin/killall"];
     for (NSString *path in candidates) {
